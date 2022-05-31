a05e5371e65a8bf7de27927de0f0d6ded6f28ae1
wofov <106087152+wofov@users.noreply.github.com>
2022-05-31 18:13:50 +0900

    Add files via upload

---
+++ ASDaSD.java
@@ -0,0 +1,18 @@
+
+public class ASDaSD {
+
+	public static void main(String[] args) {
+		/*
+		 응용문제
+		 다음 배열값이 있습니다. 해당 배열 값을 비교하여
+		 사용자 정보를 출력하시오
+		 Adata = hong kim park jang lee soun
+		 Bdata = 80 100 46 38 65 88
+		 결과배열에 따른 조건은 60점 이상(포함) 중 합격자만 출력합니다
+		 결과배열
+		 [hong, kim, lee, soun]
+		 */
+
+	}
+
+}
