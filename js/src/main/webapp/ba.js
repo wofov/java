a4f6474471153ea4f8874e29c1fd25b18f498854
wofov <106087152+wofov@users.noreply.github.com>
2022-07-21 18:11:29 +0900

    Add files via upload

---
+++ js/src/main/webapp/ba.js
@@ -0,0 +1,34 @@
+const ba = [
+    {
+     "p_title":"지마켓",
+     "p_img":"./mini/logo_quick_01.png",
+     "p_link":"https://www.gmarket.co.kr/"
+    },
+    {
+     "p_title":"옥션",
+     "p_img":"./mini/logo_quick_02.png",
+     "p_link":"http://www.auction.co.kr/"
+    },
+    {
+     "p_title":"하프클럽",
+     "p_img":"./mini/logo_quick_03.png",
+     "p_link":"http://www.halfclub.com/"
+    },
+    {
+     "p_title":"11번가",
+     "p_img":"./mini/logo_quick_04.png",
+     "p_link":"https://www.11st.co.kr/"
+    },
+    {
+     "p_title":"위메프",
+     "p_img":"./mini/logo_quick_05.png",
+     "p_link":"https://front.wemakeprice.com/"
+    },
+    {
+     "p_title":"쿠팡",
+     "p_img":"./mini/logo_quick_06.png",
+     "p_link":"https://www.coupang.com/"
+    }
+];/**
+ * 
+ */
